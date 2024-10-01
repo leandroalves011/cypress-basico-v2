ac135182dfd6c1ee959f15eda7d5b2f6b5d6214a
leandroalves011 <leandrotadeualves89@gmail.com>
2024-10-01 18:15:08 -0300

    refactoring privacy spec file using lodash lib

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,6 +1,11 @@
-it('testa a página da política de privacidade de forma independente', function(){
-    cy.visit('./src/privacy.html')
+Cypress._.times(3, function(){
 
-    cy.contains('Talking About Testing')
-    .should('be.visible')
+    it('testa a página da política de privacidade de forma independente', function(){
+        cy.visit('./src/privacy.html')
+        cy.contains('Talking About Testing')
+        .should('be.visible')
+    })
 })
+
+// Cypress._.times() = funcionalidade que repete o teste. Executa uma função de callback X vezes.
+// lodash (lib do javascript)
